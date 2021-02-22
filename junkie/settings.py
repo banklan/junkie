6d7463a6ae12f9d56a0437450e84a90f62306720
banky <banklan2010@gmail.com>
2021-02-22 12:16:40 +0100

    fixed allowed_hosts

--- junkie/settings.py
+++ junkie/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['https://kolabo.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['https://kolabo.herokuapp.com/', 'kolabo.herokuapp.com', 'kolabo.com.ng', 'localhost']
 
 
 # Application definition
