ea7cc58f05430801f65c34291e27dc601b319fff
banky <banklan2010@gmail.com>
2021-02-25 09:37:10 +0100

    fixed webpack issues7

--- junkie/settings.py
+++ junkie/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -185,6 +187,9 @@ WEBPACK_LOADER = {
         "IGNORE": [".*\.hot-update.js", ".+\.map"]
     }
 }
+
+django_heroku.settings(locals())
+
 
 AUTH_USER_MODEL = 'profiles.CustomUser'
 
